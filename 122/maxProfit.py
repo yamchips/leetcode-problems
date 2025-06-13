7657648a8501fb9e81a6c79ecaed1185b61a8c82
yamchips <wflunch@gmail.com>
2025-06-13 15:43:53 +0100

    Add DP solution to 122

--- 122/maxProfit.py
+++ 122/maxProfit.py
@@ -1,4 +1,4 @@
-def maxProfit(prices: list[int]) -> int:
+def maxProfit3(prices: list[int]) -> int:
     n = len(prices)
     if n == 1 : return 0
     profit = 0
@@ -19,4 +19,14 @@ def maxProfit2(prices:list[int]) -> int:
         if (prices[i] - prices[i - 1] > 0):
             profit += prices[i] - prices[i - 1]
     return profit
-        
+
+def maxProfit3(prices:list[int])-> int:
+    n = len(prices)
+    s0 = [0] * n
+    s1 = [0] * n
+    s1[0] = -prices[0]
+    for i in range(1, n):
+        s0[i] = max(s0[i-1], s1[i-1]+prices[i])
+        s1[i] = max(s1[i-1], s0[i-1]-prices[i])
+    return s0[-1]
+         
--- 122/test_maxProfit.py
+++ 122/test_maxProfit.py
@@ -4,23 +4,23 @@ import maxProfit
 class TestMaxProfit(unittest.TestCase):
     def test_case1(self):
         prices = [7,1,5,3,6,4]
-        self.assertEqual(7, maxProfit.maxProfit(prices))
+        self.assertEqual(7, maxProfit.maxProfit3(prices))
 
     def test_case2(self):
         prices = [1,2,3,4,5]
-        self.assertEqual(4, maxProfit.maxProfit(prices))
+        self.assertEqual(4, maxProfit.maxProfit3(prices))
 
     def test_case3(self):
         prices = [9]
-        self.assertEqual(0, maxProfit.maxProfit(prices))
+        self.assertEqual(0, maxProfit.maxProfit3(prices))
     
     def test_case4(self):
         prices = [9, 13]
-        self.assertEqual(4, maxProfit.maxProfit(prices))
+        self.assertEqual(4, maxProfit.maxProfit3(prices))
 
     def test_case5(self):
         prices = [7,6,4,3,1]
-        self.assertEqual(0, maxProfit.maxProfit(prices))
+        self.assertEqual(0, maxProfit.maxProfit3(prices))
 
 
 if __name__=='__main__':
