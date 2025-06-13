add6c32cd86118664291c52147f3ca71eb986de6
yamchips <wflunch@gmail.com>
2025-06-13 10:08:17 +0100

    Apply O(n) DP algorithm to 309

--- 309/maxProfit.py
+++ 309/maxProfit.py
@@ -1,4 +1,4 @@
-def maxProfit(prices:list[int]) -> int:
+def maxProfit2(prices:list[int]) -> int:
     n = len(prices)
     if (n == 1): return 0
     dp = [0] * n
@@ -9,7 +9,23 @@ def maxProfit(prices:list[int]) -> int:
     for i in range(2, n):
         for j in range(i):
             index = 0 if j - 2 < 0 else j - 2
-            dp[i] = max(prices[i] - prices[j] + dp[index], dp[i])
+            if (prices[i] - prices[j] > 0): 
+                dp[i] = max(prices[i] - prices[j] + dp[index], dp[i])
+            else:
+                dp[i] = max(dp[index], dp[i])        
         dp[i] = max(dp[i-1], dp[i])
     return dp[n - 1]
+
+def maxProfit2(prices:list[int]) ->int:
+    n = len(prices)
+    if (n == 1): return 0
+    s0 = [0] * n
+    s1 = [0] * n
+    s2 = [0] * n
+    s1[0] = -prices[0]
+    for i in range(1,n):
+        s0[i] = max(s2[i-1], s0[i-1])
+        s1[i] = max(s1[i-1], s0[i-1]-prices[i])
+        s2[i] = s1[i-1] + prices[i]
+    return max(s0[-1], s2[-1])
     
--- 309/test_maxProfit.py
+++ 309/test_maxProfit.py
@@ -4,23 +4,23 @@ import maxProfit
 class TestMaxProfit(unittest.TestCase):
     def test_case1(self):
         prices = [1,2,3,0,2]
-        self.assertEqual(3, maxProfit.maxProfit(prices))
+        self.assertEqual(3, maxProfit.maxProfit2(prices))
 
     def test_case2(self):
         prices = [1,2,3,4,5]
-        self.assertEqual(4, maxProfit.maxProfit(prices))
+        self.assertEqual(4, maxProfit.maxProfit2(prices))
 
     def test_case3(self):
         prices = [9]
-        self.assertEqual(0, maxProfit.maxProfit(prices))
+        self.assertEqual(0, maxProfit.maxProfit2(prices))
     
     def test_case4(self):
         prices = [9, 13]
-        self.assertEqual(4, maxProfit.maxProfit(prices))
+        self.assertEqual(4, maxProfit.maxProfit2(prices))
 
     def test_case5(self):
         prices = [7,6,4,3,1]
-        self.assertEqual(0, maxProfit.maxProfit(prices))
+        self.assertEqual(0, maxProfit.maxProfit2(prices))
 
 
 if __name__=='__main__':
