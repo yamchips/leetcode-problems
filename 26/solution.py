c9828f4f3a83082d73bf1f2e5d2fa8327253edaf
yamchips <wflunch@gmail.com>
2025-06-14 09:42:28 +0100

    Improve solution 26

--- 26/solution.py
+++ 26/solution.py
@@ -1,4 +1,4 @@
-def removeDuplicates(nums: list[int]) -> int:
+def removeDuplicates2(nums: list[int]) -> int:
     slow, fast = 0, 0
     while fast < len(nums):
         while not (nums[fast] > nums[slow]):
@@ -11,3 +11,12 @@ def removeDuplicates(nums: list[int]) -> int:
         slow += 1
         nums[slow], nums[fast] = nums[fast], nums[slow]
     return slow + 1
+
+def removeDuplicates2(nums:list[int]) -> int:
+    slow = 0
+    for fast in range(1, len(nums)):
+        if nums[fast] != nums[slow]:
+            slow += 1
+            nums[slow], nums[fast] = nums[fast], nums[slow]
+    return slow + 1
+
--- 26/test_solution.py
+++ 26/test_solution.py
@@ -4,15 +4,15 @@ import solution
 class TestSolution(unittest.TestCase):
     def test_case1(self):
         nums = [1,1,2]
-        self.assertEqual(2, solution.removeDuplicates(nums))
+        self.assertEqual(2, solution.removeDuplicates2(nums))
 
     def test_case2(self):
         nums = [0,0,1,1,1,2,2,3,3,4]
-        self.assertEqual(5, solution.removeDuplicates(nums))
+        self.assertEqual(5, solution.removeDuplicates2(nums))
 
     def test_case3(self):
         nums = [0,0,0,1,2,2]
-        self.assertEqual(3, solution.removeDuplicates(nums))
+        self.assertEqual(3, solution.removeDuplicates2(nums))
 
    
 if __name__=='__main__':
