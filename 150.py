de9f51dd4ae028a31bdcfe93bbf2b317bfad6aa9
yamchips <wflunch@gmail.com>
2025-07-14 10:31:49 +0100

    Solve 150

---
+++ 150.py
@@ -0,0 +1,24 @@
+def evalRPN(tokens: list[str]) -> int:
+    stack = []
+    for token in tokens:
+        if token in {'+', '-','*', '/'}:
+            second = int(stack.pop())
+            first = int(stack.pop())
+            res = 0
+            if token == '+':
+                res = first + second
+            elif token == '-':
+                res = first - second
+            elif token == '*':
+                res = first * second
+            else:
+                res = int(first / second)
+            stack.append(res)
+        else:
+            stack.append(token)
+    return int(stack.pop())
+
+if __name__=='__main__':
+    print(evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
+    print(evalRPN(["4","13","5","/","+"]))
+    
